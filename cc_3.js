21d203e335ee9d9f99755f349385b172ccb351f1
H Sandra Ktla <hsandraktla@usf.edu>
2025-01-30 12:16:32 -0500

    Task 2 - Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -7,3 +7,15 @@ prices.push(35);
 prices.shift();
 //Log the updated price list to the console.
 console.log("Updated Price List: ", prices);
+
+
+//Task 2: Modifying Customer Orders
+//Declare an array orders with at least five numerical values representing quantities.
+let orders = [3, 5, 7, 2, 4];
+//Increase the third order quantity by 5.
+orders[2] += 5;
+//Calculate the total number of all orders.
+let totalNumberofOrders = orders.reduce((total, quanity) => total + quanity, 0);
+//Log the updated array and total order count to the console.
+console.log("Updated Order List: ", orders);
+console.log("Total Number of Orders: ", totalNumberofOrders);
