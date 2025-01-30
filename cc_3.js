4f159e5236578082316308dba03a6faa9b2e3de9
H Sandra Ktla <hsandraktla@usf.edu>
2025-01-30 09:44:42 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,9 @@
+//Task 1: Product Price Management
+//Declare an array prices using let and initialize it with at least five numerical values.
+let prices = [15, 25, 30, 25, 10];
+//Add a new price to the array.
+prices.push(35);
+//Remove the first price from the array.
+prices.shift();
+//Log the updated price list to the console.
+console.log("Updated Price List: ", prices);
